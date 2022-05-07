7954b6c80cf339955d3ca880caabde0a906e90a2
chidiebereojingwa <ojingwaoji@gmail.com>
2022-05-07 05:01:10 +0100

    Added functions

--- funtions.test.js
+++ funtions.test.js
@@ -3,3 +3,7 @@ const functions = require('./funtions')
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2,2)).toBe(4);
 })
+
+test("Adds 2 + 2 to Not equal 4", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
