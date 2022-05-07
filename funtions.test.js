458d549857fe19f68f663ed19bcbce2c881f1d66
chidiebereojingwa <ojingwaoji@gmail.com>
2022-05-07 09:40:55 +0100

    Added functions

--- funtions.test.js
+++ funtions.test.js
@@ -36,6 +36,11 @@ test("User should be Chidiebere Ojingwa Object", () => {
 // Less than and greater than
 test('Should be under 1600', () => {
     const load1 = 800;
-    const load2 = 700;
-    expect (load1 + load2).toBeLessThan(1600)
+    const load2 = 800;
+    expect (load1 + load2).toBeLessThanOrEqual(1600)
+})
+
+// Regex
+test('There is no I in team', ()=>{
+    expect('teami').not.toMatch(/I/);
 })
