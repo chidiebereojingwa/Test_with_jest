d6f417250be5d5bbb8d5a4c18bb91b101a1bfe80
chidiebereojingwa <ojingwaoji@gmail.com>
2022-05-07 05:05:09 +0100

    Added functions

--- funtions.test.js
+++ funtions.test.js
@@ -7,3 +7,10 @@ test('Adds 2 + 2 to equal 4', () => {
 test("Adds 2 + 2 to Not equal 4", () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
