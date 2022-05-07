12aa4eab845d56a8dbb3c6ff1815d0e7beeebdc6
chidiebereojingwa <ojingwaoji@gmail.com>
2022-05-07 06:14:33 +0100

    Added functions

--- funtions.test.js
+++ funtions.test.js
@@ -32,3 +32,10 @@ test("User should be Chidiebere Ojingwa Object", () => {
     lastName: "Ojingwa",
   });
 });
+
+// Less than and greater than
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect (load1 + load2).toBeLessThan(1600)
+})
